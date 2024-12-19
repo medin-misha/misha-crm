22b91448dc64983a542317a267f4c6a30597ee74
misha <medinskijmisa228@gmail.com>
2024-12-19 15:39:39 +0200

    add models in admin panel

--- crm/app/admin.py
+++ crm/app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from .models import Service, AdCompany, Client, Contract
 # Register your models here.
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = "name", "price"
+    list_display_links = list_display
+
+
+@admin.register(AdCompany)
+class AdCompanyAdmin(admin.ModelAdmin):
+    list_display = "name", "service", "budget", "promotional_channel"
+    list_display_links = list_display
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = "full_name", "email", "phone", "is_active", "ad_company", "contract"
+    list_display_links = list_display
+
+
+@admin.register(Contract)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = "name", "service", "conclusion_date", "price"
+    list_display_links = list_display
+
+
