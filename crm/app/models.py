6988e91e738c487b28eb2da1c99ddc1175d32ce7
misha <medinskijmisa228@gmail.com>
2024-12-19 15:32:42 +0200

    add new app 'app' and new models: Service, AdCompany, Contract, Client

---
+++ crm/app/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from datetime import date
+from .utils.uploads import upload_document
+
+
+# Create your models here.
+class Service(models.Model):
+    name: str = models.CharField(max_length=200)
+    description: str = models.TextField(max_length=10000, null=True)
+    price: float = models.DecimalField(decimal_places=1, max_digits=20)
+
+    class Meta:
+        verbose_name = "service"
+
+    def __str__(self):
+        return self.name
+
+
+class AdCompany(models.Model):
+    name: str = models.CharField(max_length=200)
+    service: Service = models.ForeignKey(to=Service, on_delete=models.PROTECT)
+    promotional_channel: str = models.CharField(max_length=200, null=True)
+    budget: float = models.DecimalField(decimal_places=1, max_digits=20)
+
+    class Meta:
+        verbose_name = "ad company"
+        verbose_name_plural = "ad companies"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Contract(models.Model):
+    name: str = models.CharField(max_length=200)
+    service: Service = models.ForeignKey(to=Service, on_delete=models.CASCADE)
+    document: str = models.FileField(null=True, upload_to=upload_document)
+    conclusion_date: date = models.DateField(null=True)
+    days_of_action: int = models.PositiveIntegerField(null=True)
+    price: float = models.DecimalField(decimal_places=1, max_digits=20)
+
+    class Meta:
+        verbose_name = "contract"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Client(models.Model):
+    full_name: str = models.CharField(max_length=200)
+    phone: int = models.PositiveBigIntegerField()
+    email: str = models.EmailField()
+    is_active: bool = models.BooleanField(default=False)
+    ad_company: AdCompany = models.ForeignKey(to=AdCompany, on_delete=models.PROTECT)
+    contract: Contract = models.OneToOneField(to=Contract, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.full_name
+
+    class Meta:
+        verbose_name = "client"
--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app.apps.AppConfig",
 ]
 
 MIDDLEWARE = [
@@ -128,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
