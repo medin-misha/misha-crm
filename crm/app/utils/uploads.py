04bf925cc63b3dab63f4b228b9aca965a5bce59e
misha <medinskijmisa228@gmail.com>
2024-12-23 20:02:36 +0200

    fix path bug

--- crm/app/utils/uploads.py
+++ crm/app/utils/uploads.py
@@ -1,5 +1,5 @@
 from django.conf import settings
 
 
-def upload_document(instance: "Contract" , filename: str) -> str:
-    return f"{settings.STATIC_ROOT}/documents/{instance.name}/{filename}"
+def upload_document(instance: "Contract", filename: str) -> str:
+    return f"static/documents/{filename}"
