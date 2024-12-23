0f50a995b28e4f45cd01bfa0de4d1bc979ad3d64
misha <medinskijmisa228@gmail.com>
2024-12-23 16:48:26 +0200

    add UserIndexView

--- crm/app/urls.py
+++ crm/app/urls.py
@@ -1,3 +1,8 @@
+from django.urls import path
+from .views import UsersIndexView
+
 app_name = "app"
 
-urlpatterns = []
+urlpatterns = [
+    path("", UsersIndexView.as_view(), name="user-index")
+]
--- crm/app/views.py
+++ crm/app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Service, AdCompany, Client
+
 
 # Create your views here.
+
+class UsersIndexView(TemplateView):
+    template_name = "users/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["products_count"] = Service.objects.all().distinct().count()
+        context["advertisements_count"] = AdCompany.objects.all().distinct().count()
+        context["leads_count"] = Client.objects.filter(is_active=False).all().distinct().count()
+        context["customers_count"] = Client.objects.filter(is_active=True).all().distinct().count()
+
+        return context
