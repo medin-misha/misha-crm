b2a0af8362568d44a83aee1ca3aacccf76b24c74
misha <medinskijmisa228@gmail.com>
2024-12-23 20:03:32 +0200

    add new paths

--- crm/app/urls.py
+++ crm/app/urls.py
@@ -1,11 +1,25 @@
 from django.urls import path
-from .views import UsersIndexView, ServiceListView
+from .views import (
+    user_views,
+    ad_company_views,
+    client_views,
+    contract_views,
+    service_views,
+)
 
 app_name = "app"
 
 urlpatterns = [
-    path("", UsersIndexView.as_view(), name="user-index"),
-
-    path("products/", ServiceListView.as_view(), name="servises-list"),
-
+    path("", user_views.UsersIndexView.as_view(), name="user-index"),
+    path("products/", service_views.ServiceListView.as_view(), name="servises-list"),
+    path("ads/", ad_company_views.AdCompanyListView.as_view(), name="ads-list"),
+    path("leads/", client_views.ClientListView.as_view(), name="leads-list"),
+    path(
+        "leads/active",
+        client_views.ActiveClientListView.as_view(),
+        name="active-client-list",
+    ),
+    path(
+        "contracts/", contract_views.ContractListView.as_view(), name="contracts-list"
+    ),
 ]
