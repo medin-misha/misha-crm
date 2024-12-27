0dba65ce172cc96c0272906fcec66721e6f5f6a0
misha <medinskijmisa228@gmail.com>
2024-12-27 10:21:52 +0200

    add ad company views

--- crm/app/views/ad_company_views.py
+++ crm/app/views/ad_company_views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+from django.urls import reverse_lazy
 from ..models import Service, AdCompany, Client, Contract
 
 
@@ -6,3 +7,34 @@ class AdCompanyListView(ListView):
     template_name = "ads/ads-list.html"
     model = AdCompany
     context_object_name = "ads"
+
+
+class AdCompanyCreateView(CreateView):
+    template_name = "ads/ads-create.html"
+    model = AdCompany
+    fields = "name", "service", "promotional_channel", "budget"
+
+    def get_success_url(self):
+        return reverse_lazy("app:ads-list")
+
+
+class AdCompanyUpdateView(UpdateView):
+    template_name = "ads/ads-edit.html"
+    model = AdCompany
+    fields = "name", "service", "promotional_channel", "budget"
+
+    def get_success_url(self):
+        return reverse_lazy("app:ads-list")
+
+
+class AdCompanyDetailView(DetailView):
+    template_name = "ads/ads-detail.html"
+    model = AdCompany
+
+
+class AdCompanyDeleteView(DeleteView):
+    template_name = "ads/ads-delete.html"
+    model = AdCompany
+
+    def get_success_url(self):
+        return reverse_lazy("app:ads-list")
