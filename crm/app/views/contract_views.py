527297baaa9391c1a7feb22713fd5b1b50a7f4bc
misha <medinskijmisa228@gmail.com>
2024-12-27 10:22:44 +0200

    add contract view

--- crm/app/views/contract_views.py
+++ crm/app/views/contract_views.py
@@ -1,4 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DeleteView,
+    DetailView,
+    UpdateView,
+)
+from django.urls import reverse_lazy
 from ..models import Service, AdCompany, Client, Contract
 
 
@@ -6,3 +13,50 @@ class ContractListView(ListView):
     template_name = "contracts/contracts-list.html"
     model = Contract
     context_object_name = "contracts"
+
+
+class ContractCreateView(CreateView):
+    template_name = "contracts/contracts-create.html"
+    model = Contract
+    fields = (
+        "name",
+        "service",
+        "client",
+        "document",
+        "conclusion_date",
+        "days_of_action",
+        "price",
+    )
+
+    def get_success_url(self):
+        return reverse_lazy("app:contracts-list")
+
+
+class ContractDetailView(DetailView):
+    template_name = "contracts/contracts-detail.html"
+    model = Contract
+
+
+class ContractUpdateView(UpdateView):
+    template_name = "contracts/contracts-edit.html"
+    model = Contract
+    fields = (
+        "name",
+        "service",
+        "client",
+        "document",
+        "conclusion_date",
+        "days_of_action",
+        "price",
+    )
+
+    def get_success_url(self):
+        return reverse_lazy("app:contracts-list")
+
+
+class ContractDeleteView(DeleteView):
+    template_name = "contracts/contracts-delete.html"
+    model = Contract
+
+    def get_success_url(self):
+        return reverse_lazy("app:contracts-list")
