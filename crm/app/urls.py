1b95609db57e28b3fd43fd5d984848252f3b3b69
misha <medinskijmisa228@gmail.com>
2024-12-27 10:21:14 +0200

    add new paths

--- crm/app/urls.py
+++ crm/app/urls.py
@@ -32,7 +32,6 @@ urlpatterns = [
         service_views.ServiceUpdateView.as_view(),
         name="services-edit",
     ),
-    path("ads/", ad_company_views.AdCompanyListView.as_view(), name="ads-list"),
     path("clients/", client_views.ClientListView.as_view(), name="clients-list"),
     path(
         "clients/active",
@@ -40,6 +39,50 @@ urlpatterns = [
         name="active-client-list",
     ),
     path(
+        "clients/new/", client_views.ClientCreateView.as_view(), name="clients-create"
+    ),
+    path(
+        "clients/edit/<int:pk>",
+        client_views.ClientUpdateView.as_view(),
+        name="clients-edit",
+    ),
+    path(
+        "clients/detail/<int:pk>",
+        client_views.ClientDetailView.as_view(),
+        name="clients-detail",
+    ),
+    path(
+        "clients/delete/<int:pk>",
+        client_views.ClientDeleteView.as_view(),
+        name="clients-delete",
+    ),
+    path(
         "contracts/", contract_views.ContractListView.as_view(), name="contracts-list"
     ),
+    path(
+        "contracts/new/",
+        contract_views.ContractCreateView.as_view(),
+        name="contracts-create",
+    ),
+    path(
+        "contracts/detail/<int:pk>",
+        contract_views.ContractDetailView.as_view(),
+        name="contracts-detail",
+    ),
+    path(
+        "contracts/edit/<int:pk>",
+        contract_views.ContractUpdateView.as_view(),
+        name="contracts-edit",
+    ),
+    path(
+        "contracts/delete/<int:pk>",
+        contract_views.ContractDeleteView.as_view(),
+        name="contracts-delete",
+    ),
+
+    path("ads/", ad_company_views.AdCompanyListView.as_view(), name="ads-list"),
+    path("ads/new/", ad_company_views.AdCompanyCreateView.as_view(), name="ads-create"),
+    path("ads/detail/<int:pk>", ad_company_views.AdCompanyDetailView.as_view(), name="ads-detail"),
+    path("ads/edit/<int:pk>", ad_company_views.AdCompanyUpdateView.as_view(), name="ads-edit"),
+    path("ads/delete/<int:pk>", ad_company_views.AdCompanyDeleteView.as_view(), name="ads-delete"),
 ]
